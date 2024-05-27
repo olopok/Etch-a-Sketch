f798f5bf5dfe208ec0b8bff9f60409c580fe5334
olopok <pabisa.pb@gmail.com>
2024-05-27 12:20:18 +0100

    Default grid 16 x 16

--- script.js
+++ script.js
@@ -1,60 +1,30 @@
-const DIV = document.querySelector('.container');
-const DIV_WIDTH = DIV.offsetWidth;
-const DIV_HEIGHT = DIV.offsetHeight;
+const CONTAINER = document.querySelector('.container');
+const CONTAINER_WIDTH = CONTAINER.offsetWidth;
+const CONTAINER_HEIGHT = CONTAINER.offsetHeight;
+console.log(CONTAINER_WIDTH, CONTAINER_HEIGHT);
 
-const SQUARE_DIV = document.createElement('div');
-SQUARE_DIV.className = 'default';
-DIV.appendChild(SQUARE_DIV);
-let SQUARE_DIV_WIDTH = SQUARE_DIV.offsetWidth;
-// DIV_WIDTH - border width.
-const NUM_OF_SQUARE_ROW = (DIV_WIDTH - 4) / SQUARE_DIV_WIDTH;
-const NUM_OF_SQUARE_COL = NUM_OF_SQUARE_ROW;
+let defaultSquareNumber = 16;
+let defaultSquareWidth = (CONTAINER_WIDTH - 4) / defaultSquareNumber;
+let defaultSquareHeight = (CONTAINER_WIDTH -4) / defaultSquareNumber;
+console.log(defaultSquareNumber, defaultSquareWidth, defaultSquareHeight);
 
-let NUM_OF_SQUARE = NUM_OF_SQUARE_ROW * NUM_OF_SQUARE_COL;
-console.log(NUM_OF_SQUARE);
-
-function createDefaultGrid(row) {
-    for (let i = 1; i < row; i++) {
-        const SQUARE_DIV = document.createElement('div');
-        SQUARE_DIV.className = 'default';
-        DIV.appendChild(SQUARE_DIV);        
+function grid(squareN, squareW, squareH) {
+    for (i = 0; i <= squareN * squareN; i++) {
+        const DIV = document.createElement('div');
+        DIV.className = 'square';
+        DIV.style.width = `${squareW + 'px'}`;
+        DIV.style.height = `${squareH + 'px'}`;
+        CONTAINER.appendChild(DIV);
     }
 }
 
-createDefaultGrid(NUM_OF_SQUARE);
+grid(defaultSquareNumber, defaultSquareWidth, defaultSquareHeight);
 
-function hovering(e) {
+function hovering(e){
     e.target.style.backgroundColor = 'black';
 }
 
-const SQUARE = document.querySelectorAll('.default');
-for (const sqr of SQUARE){
-    sqr.addEventListener('mouseover', hovering)
+let div = document.querySelectorAll('.square');
+for (const d of div) {
+    d.addEventListener('mouseover', hovering);
 }
-
-function Grid() {
-    NUM_OF_SQUARE = prompt("How many squares per side? \n(max 100)");
-    SQUARE_DIV_WIDTH = (DIV_WIDTH - 4) / NUM_OF_SQUARE;
-    NUM_OF_SQUARE *= NUM_OF_SQUARE;
-    console.log(SQUARE_DIV_WIDTH);
-    while (DIV.hasChildNodes()) {
-        DIV.removeChild(DIV.firstChild);
-      }
-
-      for (let i = 1; i <= NUM_OF_SQUARE; i++) {
-        const SQUARE_DIV = document.createElement('div');
-        SQUARE_DIV.className = 'newSquare';
-        SQUARE_DIV.style.width = SQUARE_DIV_WIDTH + 'px';
-        SQUARE_DIV.style.height = SQUARE_DIV_WIDTH + 'px';
-        DIV.appendChild(SQUARE_DIV);        
-    }
-
-    const SQUARE = document.querySelectorAll('.newSquare');
-for (const sqr of SQUARE){
-    sqr.addEventListener('mouseover', hovering)
-}
-}
-
-const BUTTON = document.querySelector('#newGrid');
-BUTTON.addEventListener('click', Grid);
-
