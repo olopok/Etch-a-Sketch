88bb5c30d81b1de8385c5745a958ca74b05f30c2
olopok <pabisa.pb@gmail.com>
2024-05-27 10:25:43 +0100

    Activate new grid button

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const DIV = document.querySelector('.container');
 const DIV_WIDTH = DIV.offsetWidth;
 const DIV_HEIGHT = DIV.offsetHeight;
+
 const SQUARE_DIV = document.createElement('div');
 SQUARE_DIV.className = 'default';
 DIV.appendChild(SQUARE_DIV);
-const SQUARE_DIV_WIDTH = SQUARE_DIV.offsetWidth;
+let SQUARE_DIV_WIDTH = SQUARE_DIV.offsetWidth;
 // DIV_WIDTH - border width.
 const NUM_OF_SQUARE_ROW = (DIV_WIDTH - 4) / SQUARE_DIV_WIDTH;
 const NUM_OF_SQUARE_COL = NUM_OF_SQUARE_ROW;
-const NUM_OF_SQUARE = NUM_OF_SQUARE_ROW * NUM_OF_SQUARE_COL;
+
+let NUM_OF_SQUARE = NUM_OF_SQUARE_ROW * NUM_OF_SQUARE_COL;
 console.log(NUM_OF_SQUARE);
 
 function createDefaultGrid(row) {
@@ -29,3 +31,30 @@ const SQUARE = document.querySelectorAll('.default');
 for (const sqr of SQUARE){
     sqr.addEventListener('mouseover', hovering)
 }
+
+function Grid() {
+    NUM_OF_SQUARE = prompt("How many squares per side? \n(max 100)");
+    SQUARE_DIV_WIDTH = (DIV_WIDTH - 4) / NUM_OF_SQUARE;
+    NUM_OF_SQUARE *= NUM_OF_SQUARE;
+    console.log(SQUARE_DIV_WIDTH);
+    while (DIV.hasChildNodes()) {
+        DIV.removeChild(DIV.firstChild);
+      }
+
+      for (let i = 1; i <= NUM_OF_SQUARE; i++) {
+        const SQUARE_DIV = document.createElement('div');
+        SQUARE_DIV.className = 'newSquare';
+        SQUARE_DIV.style.width = SQUARE_DIV_WIDTH + 'px';
+        SQUARE_DIV.style.height = SQUARE_DIV_WIDTH + 'px';
+        DIV.appendChild(SQUARE_DIV);        
+    }
+
+    const SQUARE = document.querySelectorAll('.newSquare');
+for (const sqr of SQUARE){
+    sqr.addEventListener('mouseover', hovering)
+}
+}
+
+const BUTTON = document.querySelector('#newGrid');
+BUTTON.addEventListener('click', Grid);
+
